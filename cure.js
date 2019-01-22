2905d9f682fbec405e764590721f057e4f54feb6
BG <Bgee269@hotmail.com>
2019-01-21 23:56:45 -0500

    new look

---
+++ cure.js
@@ -0,0 +1,9 @@
+
+
+$.ajax({
+    url: "https://cors-anywhere.herokuapp.com/https://mmjmenu.com/api/v1/menu_items",
+    headers: {"Authorization": "Basic " + btoa("api_token:saNoWSwPGchzFyxreZlFPpaws")},
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });
