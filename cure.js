1c5f469ca6241b7757895a1656d883e883d5ff06
BG <Bgee269@hotmail.com>
2019-01-24 15:38:35 -0500

    new page

--- cure.js
+++ cure.js
@@ -1,5 +1,3 @@
-
-
 $.ajax({
     url: "https://cors-anywhere.herokuapp.com/https://mmjmenu.com/api/v1/menu_items",
     headers: {"Authorization": "Basic " + btoa("api_token:saNoWSwPGchzFyxreZlFPpaws")},
