b0d85abbbfbbd05c25a5550678d26e222e38bff9
BG <Bgee269@hotmail.com>
2019-02-09 15:13:30 -0500

    update

--- cure.js
+++ cure.js
@@ -1,5 +1,8 @@
+// $ curl -u saNoWSwPGchzFyxreZlFPpaws:x https://mmjmenu.com/api/v1/menu_items
+
 $.ajax({
-    url: "https://cors-anywhere.herokuapp.com/https://mmjmenu.com/api/v1/menu_items",
+    // url: "https://cors-anywhere.herokuapp.com/https://mmjmenu.com/api/v1/menu_items",
+    url: "https://mmjmenu.com/api/v1/menu_items/",
     headers: {"Authorization": "Basic " + btoa("api_token:saNoWSwPGchzFyxreZlFPpaws")},
     method: "GET"
   }).then(function(response) {
