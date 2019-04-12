0f6aea856edbbc27a32f398fd1e7ab28ded0897b
BG <Bgee269@hotmail.com>
2019-04-11 20:48:09 -0400

    working cure page!

--- cure.js
+++ cure.js
@@ -1,10 +1,21 @@
 // $ curl -u saNoWSwPGchzFyxreZlFPpaws:x https://mmjmenu.com/api/v1/menu_items
 
-$.ajax({
-    // url: "https://cors-anywhere.herokuapp.com/https://mmjmenu.com/api/v1/menu_items",
-    url: "https://mmjmenu.com/api/v1/menu_items/",
-    headers: {"Authorization": "Basic " + btoa("api_token:saNoWSwPGchzFyxreZlFPpaws")},
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-  });
+
+  // On click search button display menu items
+
+  // parse json menu to display on front end
+
+  // split every item into arrays based on ailment
+  // var insomnia = "dream tincture", "707 headaband", "10 mg capsules"
+  // var depression = "etc"
+  // query items based on that
+  
+  // brings you to a survey that displays items based on answers
+  
+  // wish list connected to database
+
+  // buttons that output the arrays
+  // buttons are labeled by ailment
+  // choose top 10 most prevelant of the 21 ailments. autism, cancer etc.
+  // add how to's for products
+  // use smiley faces as buttons
