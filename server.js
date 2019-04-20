64d66af59c955db8870e12d86c8f4f58486a3e49
Brandon Gee <43019538+bgee269@users.noreply.github.com>
2019-04-20 14:01:23 -0400

    Sidebranch (#4)
    * mysql
    
    * green

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ app.use(express.json());
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-//require("./routes/apiRoutes")(app);
-//require("./routes/htmlRoutes")(app);
+require("./routes/api-routes")(app);
+//require("./app/config/connection")(app);
 
 // =============================================================================
 // LISTENER
