b10b885c831a85b6ffddb40862f31b858b38817d
BG <Bgee269@hotmail.com>
2019-04-23 20:50:50 -0400

    jaws4

--- database.js
+++ database.js
@@ -3,18 +3,10 @@ var mysql = require("mysql");
 // Set up our connection information
 var connection = mysql.createConnection({
   port: 3306,
-<<<<<<< HEAD
-=======
-
->>>>>>> 73b14fb2cb5c99a89e1cfff72324aaa680b9e8aa
   host: "bbj31ma8tye2kagi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   user: "dvi6ll60oa36r1r9",
   password: "dzbue5r1ffr917p3",
   database: "ogb3reih9ah9fyrn"
-<<<<<<< HEAD
-=======
-
->>>>>>> 73b14fb2cb5c99a89e1cfff72324aaa680b9e8aa
 });
 
 // Connect to the database
