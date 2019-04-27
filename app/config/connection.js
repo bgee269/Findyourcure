36ff9909c19efce79a900f741508dcc23c5199f9
Brandon Gee <43019538+bgee269@users.noreply.github.com>
2019-04-27 11:51:41 -0400

    Side4 (#10)
    * mysql
    
    * green
    
    * jaws
    
    * jaws update
    
    * updated jaws
    
    * jaws4
    
    * update mongo database

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,3 +1,20 @@
+<<<<<<< HEAD
+//var mysql = require("mysql");
+//require('dotenv').config();
+//var connection = mysql.createConnection("mysql://dvi6ll60oa36r1r9:dzbue5r1ffr917p3@bbj31ma8tye2kagi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ogb3reih9ah9fyrn");
+var Sequelize = require("sequelize");
+
+var sequelize = new Sequelize("sequelize_chirpy1", "root", "", {
+  host: "bbj31ma8tye2kagi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  port: 3306,
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+=======
 var mysql = require("mysql");
 require('dotenv').config();
 var connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -11,15 +28,26 @@ var connection = mysql.createConnection(process.env.JAWSDB_URL);
 //   password: "dzbue5r1ffr917p3",
 //   database: "ogb3reih9ah9fyrn"
 // });
+>>>>>>> 5a0cf2dc2f2dc7b138f4309ae12b9d60765c39c0
+
+module.exports = sequelize;
+// Set up our connection information
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   hostname: "bbj31ma8tye2kagi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//   username: "dvi6ll60oa36r1r9",
+//   password: "dzbue5r1ffr917p3",
+//   database: "ogb3reih9ah9fyrn"
+// });
 
 // Connect to the database
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+//connection.connect(function(err) {
+  //if (err) {
+    //console.error("error connecting: " + err.stack);
+    //return;
+  //}
+  //console.log("connected as id " + connection.threadId);
+//});
 
 // Export connection
-module.exports = connection;
+//module.exports = connection;
